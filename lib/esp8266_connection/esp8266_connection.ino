a239544c78616f9dd3ae3595a4cb4bae873ae784
NesInf <61068383+NesInf@users.noreply.github.com>
2021-10-19 19:05:07 -0500

    Reading Arduino Stuff

--- lib/esp8266_connection/esp8266_connection.ino
+++ lib/esp8266_connection/esp8266_connection.ino
@@ -1,29 +1,31 @@
 #include <WebSocketsServer.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <DHT.h>
 #include <SoftwareSerial.h>
 
 const char *ssid = "Connectify-me";
-const char *pass = "kadajuk4"
+const char *pass = "kadajuk4";
 
 String json;
+int voltage = A0;
+int voltageValue = 0;
+char vtr[10];
 
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
   String cmd = "";
   switch(type) {
-    case websocket_disconnected:
+    case WStype_DISCONNECTED:
         Serial.println("Websocket is not connected");
         break;
-    case websocket_connected:
+    case WStype_CONNECTED:
         {
           Serial.println("Websocket is connected");
           Serial.println(webSocket.remoteIP(num).toString());
           webSocket.sendTXT(num, "connected");
         }
-    case websocket_text:
+    case WStype_TEXT:
         cmd = "";
         for (int i = 0; i < length; i++) {
           cmd = cmd + (char)payload[i];
@@ -33,23 +35,24 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
         if (cmd == "readdata"){
         }
 
-        webSocket.sendTXT(num, cmd+":success");
+        webSocket.sendTXT(num, cmd);
         break;
-    case websocket_fragment_text_start:
+    case WStype_FRAGMENT_TEXT_START:
         break;
-    case websocket_fragment_bin_start:
+    case WStype_FRAGMENT_BIN_START:
         break;
-    case websocket_bin:
+    case WStype_BIN:
         hexdump(payload, length);
         break;
     default:
         break;
+  }
 }
 
 String charactersToString(char* chr) {
   String rval;
-  for(int x = 0; x < strlen(char); x++) {
-    rval = rval + char[x];
+  for(int x = 0; x < strlen(chr); x++) {
+    rval = rval + chr[x];
   }
   return rval;
 }
@@ -72,6 +75,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   webSocket.loop();
+  voltageValue = analogRead(voltage);
 
   delay(1000);
+
+  if(isnan(voltageValue))
+  {
+    Serial.println(F("Failed to read voltage value"));
+    return;
+  } else {
+    sprintf(vtr, "%.2f", voltageValue);
+
+    json = "{'voltage':'" + charactersToString(vtr) + "'}";
+    Serial.println(json);
+    webSocket.broadcastTXT(json);
+  }
 }
--- lib/screens/choose_measure.dart
+++ lib/screens/choose_measure.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'voltage_screen.dart';
 import 'current_screen.dart';
 import 'resistance_screen.dart';
 import 'data_screen.dart';
+import 'package:web_socket_channel/io.dart';
 
 class ChooseMeasure extends StatefulWidget {
   @override
@@ -10,6 +12,60 @@ class ChooseMeasure extends StatefulWidget {
 }
 
 class _ChooseMeasureState extends State<ChooseMeasure> {
+  String voltage = '';
+
+  late IOWebSocketChannel channel;
+  bool connected = false;
+
+  @override
+  void initState() {
+    connected = false;
+    voltage = "0";
+    Future.delayed(Duration.zero, () async {
+      channelconnect();
+    });
+
+    super.initState();
+  }
+
+  channelconnect() {
+    try {
+      channel = IOWebSocketChannel.connect("ws://192.168.0.1:81");
+      channel.stream.listen((message) {
+        print(message);
+        setState(() {
+          if (message == "connected") {
+            connected = true;
+          } else if (message.substring(0, 6) == "{'voltage") {
+            message = message.replaceAll(RegExp("'"), '"');
+            Map<String, dynamic> jsondata = json.decode(message);
+            setState(() {
+              voltage = jsondata["voltageValue"];
+            });
+          }
+        });
+      }, onDone: () {
+        print("Web Socket is closed");
+        setState(() {
+          connected = false;
+        });
+      }, onError: (error) {
+        print(error.toString());
+      });
+    } catch (_) {
+      print("Error on connecting to websocket.");
+    }
+
+    Future<void> sendcmd(String cmd) async {
+      if (connected == true) {
+        channel.sink.add(cmd);
+      } else {
+        channelconnect();
+        print("Web Socket is not connected");
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -107,6 +163,7 @@ class _ChooseMeasureState extends State<ChooseMeasure> {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => DataScreen()));
                 }),
+            Text("Voltage: $voltage"),
             Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 0))
           ],
         ),
